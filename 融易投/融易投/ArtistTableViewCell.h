e415d832a03af839d8dcd13e899d3aff21306b19
leecmm <kbkyan@163.com>
2016-04-07 18:05:10 +0800

    排行模块

---
+++ 融易投/融易投/ArtistTableViewCell.h
@@ -0,0 +1,18 @@
+//
+//  ArtistTableViewCell.h
+//  融易投
+//
+//  Created by efeiyi on 16/4/7.
+//  Copyright © 2016年 dongxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ArtistModel;
+
+@interface ArtistTableViewCell : UITableViewCell
+
+
+@property (nonatomic, strong) ArtistModel *model;
+
+@end
---
+++ 融易投/融易投/InvestorTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  InvestorTableViewCell.h
+//  融易投
+//
+//  Created by efeiyi on 16/4/7.
+//  Copyright © 2016年 dongxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class InvestorModel;
+
+@interface InvestorTableViewCell : UITableViewCell
+
+@property (nonatomic, strong) InvestorModel *model;
+
+@end
