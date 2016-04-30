3a9917d889e367ef7057c1ba310797c9ad3b4183
leecmm <kbkyan@163.com>
2016-04-30 09:01:31 +0800

    test

--- 融易投/融易投/MeHeaderView.m
+++ 融易投/融易投/MeHeaderView.m
@@ -15,13 +15,6 @@
     return [[[NSBundle mainBundle] loadNibNamed:NSStringFromClass(self) owner:nil options:nil] lastObject];
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
 - (IBAction)btnClick:(id)sender {
     
     SSLog(@"111");
