b43ffd082b104a0f55871bb7750f6ee5b798ad55
lipengfei <281005001@qq.com>
2016-05-24 16:42:44 +0800

    未读标签

--- 融易投/融易投/CreationTableViewController.m
+++ 融易投/融易投/CreationTableViewController.m
@@ -47,13 +47,6 @@ static NSString *ID = @"creationCell";
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.lastpageIndex = @"1";
-    //内边距的顶部应该是导航条的最大值64加上标题栏的高度
-    //    UIEdgeInsetsMake(<#CGFloat top#>, <#CGFloat left#>, <#CGFloat bottom#>, <#CGFloat right#>)
-    //    self.tableView.contentInset = UIEdgeInsetsMake(BSNavMaxY + BSTitlesViewH, 0, 0, 0);
-    //    self.tableView.scrollIndicatorInsets = UIEdgeInsetsMake(BSNavMaxY + BSTitlesViewH, 0, 0, 0);
-    //运行程序,发现系统帮我们自动调整scrollView的内边距,所以在精华控制器中设置automaticallyAdjustsScrollViewInsets = NO即可
-    //设置tableView的内边距---实现全局穿透让tableView向上移动64 + 标题栏的高度35/向下移动tabBar的高度49
-    //运行程序,发现底部一致到了tabBar的最下面,我们应该设置成子控制器的view的显示范围为tabBar的上面
     //同样,tabBar的高度我们也可能项目中都会用到,写在常量文件中
     self.tableView.contentInset = UIEdgeInsetsMake(SSStatusMaxH + SSTitlesViewH, 0, SSTabBarH, 0);
     //运行程序,发现滚动条上部分被标题栏和导航栏挡住了,这样会对会用造成一定的假象,造成对内容的多少判断不准确
@@ -73,8 +66,6 @@ static NSString *ID = @"creationCell";
 
 -(void)setUpRefresh
 {
-    //但是如果我们想整个项目都要用到上拉刷新和下拉刷新呢,不能把这上面的代码一个个拷贝了吧
-    //这样,我们可以使用继承,自定义刷新控件然后继承自MJRefreshNormalHeader,这里是自定义下拉刷新
     CommonHeader *header = [CommonHeader headerWithRefreshingBlock:^{
         [self loadNewData];
     }];
@@ -82,7 +73,7 @@ static NSString *ID = @"creationCell";
     //让程序一开始就加载数据
     [self.tableView.mj_header beginRefreshing];
     //同样,自定义上拉刷新
-    self.tableView.mj_footer = [CommonFooter footerWithRefreshingTarget:self refreshingAction:@selector(loadMoreData)];
+    self.tableView.mj_footer = [CommonFooter footerWithRefreshingTarget:self refreshingAction:@selector(loadMoreData1)];
 }
 
 
@@ -109,7 +100,7 @@ static NSString *ID = @"creationCell";
     }];
 }
 
--(void)loadMoreData{
+-(void)loadMoreData1{
     [self.tableView.mj_header endRefreshing];
     //参数
     NSString *pageSize = @"20";
@@ -124,7 +115,7 @@ static NSString *ID = @"creationCell";
                            };
     [[HttpRequstTool shareInstance] loadData:POST serverUrl:@"artWorkCreationList.do" parameters:json showHUDView:self.view andBlock:^(id respondObj) {
         //5. 解析从服务器获取的JSON数据
-//                NSString *jsonString = [[NSString alloc] initWithData:respondObj encoding:NSUTF8StringEncoding];
+                NSString *jsonString = [[NSString alloc] initWithData:respondObj encoding:NSUTF8StringEncoding];
         NSDictionary *modelDict = [NSJSONSerialization JSONObjectWithData:respondObj options:kNilOptions error:nil];
         //13. 字典数组 -> 模型数组
         NSArray *moreModels = [CreationModel mj_objectArrayWithKeyValuesArray:modelDict[@"objectList"]];
@@ -132,6 +123,7 @@ static NSString *ID = @"creationCell";
         [self.models addObjectsFromArray:moreModels];
         [[NSOperationQueue mainQueue] addOperationWithBlock:^{
             [self.tableView reloadData];
+            [self.tableView.mj_footer endRefreshing];
         }];
     }];
 }
