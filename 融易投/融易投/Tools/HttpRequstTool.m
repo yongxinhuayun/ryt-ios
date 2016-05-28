f6b29a9ca4abe959d242bc615892c7232d7a120c
lipengfei <281005001@qq.com>
2016-05-28 15:25:26 +0800

    1

--- 融易投/融易投/Tools/HttpRequstTool.m
+++ 融易投/融易投/Tools/HttpRequstTool.m
@@ -21,10 +21,12 @@ static HttpRequstTool *requstTool=nil ;
 
 @implementation HttpRequstTool
 //static  NSString *basePath = @"http://192.168.1.75:8080/app/";
-//static  NSString *basePath = @"http://192.168.1.41:8080/app/";
 //static  NSString *basePath = @"http://192.168.1.75:8001/app/";
-static  NSString *basePath = @"http://192.168.1.75:8080/app/";
+
 //static  NSString *basePath = @"http://192.168.1.41:8080/app/";
+//static  NSString *basePath = @"http://192.168.1.41:8080/app/";
+// 阿里云服务器
+static NSString *basePath = @"http://craft.efeiyi.com/app-wikiServer/app/";
 +(HttpRequstTool *)shareInstance
 {
     if (!requstTool ) {
